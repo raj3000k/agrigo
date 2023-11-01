d9dafc6966edbbf0fc2aea5eb2d993cdf40ef98d
Raj Motwani <rajmotwani38@gmail.com>
2023-11-01 23:29:52 +0530

    order page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './tailwind.css';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-import Welcome from './pages/Welcome';
-import Transporter from './pages/Transporter';
-import Distributor from './pages/Distributor';
+import logo from "./logo.svg";
+import "./tailwind.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Transporter from "./pages/Transporter";
+import Distributor from "./pages/Distributor";
+import Order from "./pages/Order";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<Welcome />}></Route>
         <Route exact path="/transporter" element={<Transporter />}></Route>
         <Route exact path="/distributor" element={<Distributor />}></Route>
+        <Route exact path="/order" element={<Order />}></Route>
       </Routes>
     </div>
   );
