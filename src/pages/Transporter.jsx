543ee61b9afb1f8b08c9f48b25500a6ed4843ef0
Harsh Agrawal <harsh.mobile231@gmail.com>
2023-11-01 19:30:39 +0530

    ui

--- src/pages/Transporter.jsx
+++ src/pages/Transporter.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState} from 'react'
 import { supabase } from '../supabase/supabase'
 import { AiOutlineMenu } from "react-icons/ai";
 import TransportDrawer from '../components/TransportDrawer';
+import LongCardImage from '../components/LongCard';
+import Navbar from '../components/Navbar';
 const Transporter=()=> {
     const[drawer,setDrawer]=useState(false)
  
     
   return (
     <div className='bg-[#d8e6ed] h-full flex flex-col'>
-        <div className='bg-[#ffde59] w-full h-auto py-2 flex flex-row justify-between px-4' >
+        {/* <div className='bg-[#045930] w-full h-auto py-2 flex flex-row justify-between px-4' >
             <div className='flex flex-row'>
 
                <button>
@@ -18,7 +20,8 @@ const Transporter=()=> {
         <h1 className='text-black font-bold text-2xl justify-items-start ml-4'>User</h1>
             </div>
         <h1 className='text-black font-bold ml-4 text-2xl mr-2'>Transporter</h1>
-      </div>
+      </div> */}
+      <Navbar/>
       {drawer?
        <>
        <TransportDrawer/>
@@ -26,8 +29,8 @@ const Transporter=()=> {
        :<></>}
        <div className='flex flex-row mt-6 p-8 justify-evenly h-full'>
         <div className='flex flex-col space-y-4 h-full'>
-            <div className='bg-[#ffde59] w-auto h-auto p-4 rounded-xl font-bold items-center'>
-                <h1 className='font-bold justify-items-center'>Orders</h1>
+            <div className='bg-[#045930] w-auto h-auto p-4 rounded-xl font-bold items-center'>
+                <h1 className='font-bold justify-items-center text'>Orders</h1>
             </div>
             <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold flex flex-col space-y-2'>
                <button>
@@ -90,7 +93,7 @@ const Transporter=()=> {
         </div>
        </div>
        
-        
+        <LongCardImage field1={'Name'} value1={"Lekhram"}/>
     </div>
   ) 
 }
