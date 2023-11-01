461535d7201ee4f4373da16505139140046fc2d2
Raj Motwani <rajmotwani38@gmail.com>
2023-11-01 23:24:21 +0530

    Distributor added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './tailwind.css';
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Transporter from './pages/Transporter';
+import Distributor from './pages/Distributor';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Welcome/>}></Route>
-        <Route exact path="/transporter" element={<Transporter/>}></Route>
+        <Route exact path="/" element={<Welcome />}></Route>
+        <Route exact path="/transporter" element={<Transporter />}></Route>
+        <Route exact path="/distributor" element={<Distributor />}></Route>
       </Routes>
     </div>
   );
