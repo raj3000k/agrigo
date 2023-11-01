bc23fdbc92b3e9c8093717df3e51155d2009cac7
Harsh Agrawal <harsh.mobile231@gmail.com>
2023-11-01 16:36:31 +0530

    UI

--- src/pages/Transporter.jsx
+++ src/pages/Transporter.jsx
@@ -7,7 +7,7 @@ const Transporter=()=> {
  
     
   return (
-    <div className='bg-[#48626e] h-full'>
+    <div className='bg-[#d8e6ed] h-full flex flex-col'>
         <div className='bg-[#ffde59] w-full h-auto py-2 flex flex-row justify-between px-4' >
             <div className='flex flex-row'>
 
@@ -24,11 +24,72 @@ const Transporter=()=> {
        <TransportDrawer/>
        </>
        :<></>}
-       <div className='mt-6 flex flex-col '>
-        <div>
-            <div cla></div>
+       <div className='flex flex-row mt-6 p-8 justify-evenly h-full'>
+        <div className='flex flex-col space-y-4 h-full'>
+            <div className='bg-[#ffde59] w-auto h-auto p-4 rounded-xl font-bold items-center'>
+                <h1 className='font-bold justify-items-center'>Orders</h1>
+            </div>
+            <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold flex flex-col space-y-2'>
+               <button>
+               <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center hover:bg-[#74dfab]'>
+                    <h1>Rice- 5Ton</h1>
+                    <h1>Location</h1>
+                </div>
+               </button>
+               <button>
+               <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center  hover:bg-[#74dfab]'>
+                    <h1>ITEM</h1>
+                    <h1>Quantity</h1>
+                    <h1>Location</h1>
+                </div>
+               </button>
+            </div>
+            
+        </div>
+        <div className='flex flex-col space-y-4 h-full '>
+            <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold items-center'>
+                <h1 className='font-bold justify-items-center'>Available Vehicles</h1>
+            </div>
+            <div className='bg-[#ffde59] w-auto h-auto p-4 rounded-xl font-bold flex flex-col space-y-2'>
+               <button>
+               <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center  hover:bg-[#74dfab]'>
+                    <h1>Tata Ace: CG 04 HX 1234</h1>
+                    
+                </div>
+               </button>
+               <button>
+               <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center  hover:bg-[#74dfab]'>
+               <h1>Tata Ace: CG 04 HX 1234</h1>
+                    
+                </div>
+               </button>
+            </div>
+            
+        </div>
+        <div className='flex flex-col space-y-4 h-full '>
+            <div className='bg-[#ffde59] w-auto h-auto p-4 rounded-xl font-bold items-center'>
+                <h1 className='font-bold justify-items-center'>In Transit</h1>
+            </div>
+            <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold flex flex-col space-y-2'>
+               <button>
+               <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center  hover:bg-[#74dfab]'>
+                    <h1>350 Kg</h1>
+                    <h1>RICE</h1>
+                    <h1>Raipur-Delhi View Details..</h1>
+                </div>
+               </button>
+               <button>
+               <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center  hover:bg-[#74dfab]'>
+               <h1>1 Ton</h1>
+                    <h1>RICE</h1>
+                    <h1>Raipur-Delhi View Details..</h1>
+                </div>
+               </button>
+            </div>
+            
         </div>
        </div>
+       
         
     </div>
   ) 
