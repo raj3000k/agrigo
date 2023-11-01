f33794b91ad31205214b56974a43a927a11fdda6
Harsh Agrawal <harsh.mobile231@gmail.com>
2023-11-01 22:09:56 +0530

    Transporter update

--- src/components/LongCard.jsx
+++ src/components/LongCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import logo from '../assets/images/logo-agri.png'
-const LongCardImage=({field1,field2,field3,field4,field5,value1,value2,value3,value4,value5})=> {
+
+
+export const LongCardImage=({field1,field2,field3,field4,field5,value1,value2,value3,value4,value5})=> {
     
   return (
-    <div className='bg-[#ffde59] w-[70%] h-auto rounded-xl p-4 m-4'>
+    <div className='bg-[#ffde59] w-full h-auto rounded-xl p-4 m-4'>
         <div className='flex flex-row justify-between items-center'>
         <img src={logo} className='h-32 w-32'/>
                 <div className='flex flex-col'>
@@ -30,28 +32,30 @@ const LongCardImage=({field1,field2,field3,field4,field5,value1,value2,value3,va
   )
 }
 
-const LongCardTransit=({field1,field2,field3,field4,field5,value1,value2,value3,value4,value5})=> {
+export const LongCardTransit=({field1,field2,field3,field4,field5,field6,value1,value2,value3,value4,value5,value6})=> {
     
     return (
-      <div className='bg-[#ffde59] w-[70%] h-auto rounded-xl p-4 m-4'>
+      <div className='bg-[#00bf63] h-auto rounded-xl p-4 m-4 hover:bg-[#045930]'>
           <div className='flex flex-row justify-between items-center'>
           <img src={logo} className='h-32 w-32'/>
                   <div className='flex flex-col'>
-                  <h1 className='text-black font-bold'>{field1}: {value1}</h1>
-                  <h1 className='text-black font-bold'>{field1}: {value1}</h1>
+                  <h1 className='text-white font-bold text-lg'>{field1}: {value1}</h1>
+                  <h1 className='text-white font-bold text-lg'>{field2}: {value2}</h1>
+                  <h1 className='text-white font-bold text-lg'>{field3}: {value3}</h1>
               </div>
               <div className='flex flex-row justify-between'>
               
               <div className='flex flex-col'>
-                  <h1 className='text-black font-bold'>{field1}: {value1}</h1>
-                  <h1 className='text-black font-bold'>{field1}: {value1}</h1>
+                  <h1 className='text-white font-bold text-lg'>{field4}: {value4}</h1>
+                  <h1 className='text-white font-bold text-lg'>{field5}: {value5}</h1>
+                  <h1 className='text-white font-bold text-lg'>{field6}: {value6}</h1>
               </div>
               </div>
               <div className='flex flex-row justify-between'>
               
               <div className='flex flex-col space-y-2'>
-              <button className='bg-[#5271ff] p-2 rounded-2xl text-white font-bold'>Verify Driver</button>
-              <button className='bg-red-400 p-2 rounded-2xl text-white font-bold'>Delete Driver</button>
+              <button className='bg-[#5271ff] p-2 rounded-2xl text-white font-bold hover:bg-blue-800'>Track Status</button>
+              <button className='bg-[#5271ff] p-2 rounded-2xl text-white font-bold hover:bg-blue-800'>Complete Detail</button>
               </div>
               </div>  
   
@@ -60,4 +64,3 @@ const LongCardTransit=({field1,field2,field3,field4,field5,value1,value2,value3,
     )
   }
   
-export default {LongCardImage,LongCardTransit}
--- src/pages/Transporter.jsx
+++ src/pages/Transporter.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState} from 'react'
 import { supabase } from '../supabase/supabase'
 import { AiOutlineMenu } from "react-icons/ai";
 import TransportDrawer from '../components/TransportDrawer';
-import LongCardImage from '../components/LongCard';
+import LongCardImage, { LongCardTransit } from '../components/LongCard';
 import Navbar from '../components/Navbar';
+
 const Transporter=()=> {
     const[drawer,setDrawer]=useState(false)
  
@@ -28,11 +29,11 @@ const Transporter=()=> {
        </>
        :<></>}
        <div className='flex flex-row mt-6 p-8 justify-evenly h-full flex-wrap'>
-        <div className='flex flex-col space-y-4 h-full'>
+        <div className='flex flex-col space-y-4 h-full w-[40%]'>
             <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold items-center flex justify-center'>
                 <h1 className='font-bold justify-items-center text-white'>Orders</h1>
             </div>
-            <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold flex flex-row space-x-2 '>
+            <div className='bg-[#00bf63] h-auto p-4 rounded-xl font-bold flex justify-center space-x-2 '>
                <button>
                <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center hover:bg-[#74dfab]'>
                     <h1>Rice- 5Ton</h1>
@@ -50,7 +51,7 @@ const Transporter=()=> {
             <button className='bg-[#5271ff] h-auto w-auto rounded-xl text-white font-bold p-2 hover:bg-blue-800'>View All Orders</button>
             
         </div>
-        <div className='flex flex-col space-y-4 h-full flex-wrap'>
+        <div className='flex flex-col space-y-4 h-full flex-wrap w-[40%]'>
             <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold items-center flex justify-center'>
                 <h1 className='font-bold justify-items-center text-white items-centre flex'>Available Vehicles</h1>
             </div>
@@ -70,32 +71,23 @@ const Transporter=()=> {
             </div>
             <button className='bg-[#5271ff] h-auto w-auto rounded-xl text-white font-bold p-2 hover:bg-blue-800'>Manage Vehicles</button>
         </div>
-        <div className='flex flex-col space-y-4 h-full flex-wrap'>
-            <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold justify-center flex items-centre'>
-                <h1 className='font-bold justify-items-center text-white items-centre flex'>In Transit</h1>
-            </div>
-            <div className='bg-[#00bf63] w-auto h-auto p-4 rounded-xl font-bold flex flex-row space-x-2'>
-               <button>
-               <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center  hover:bg-[#74dfab]'>
-                    <h1>350 Kg</h1>
-                    <h1>RICE</h1>
-                    <h1>Raipur-Delhi View Details..</h1>
-                </div>
-               </button>
-               <button>
-               <div className='bg-[#d8e6ed] w-auto h-auto p-4 rounded-xl font-bold flex flex-col justify-center items-center  hover:bg-[#74dfab]'>
-               <h1>1 Ton</h1>
-                    <h1>RICE</h1>
-                    <h1>Raipur-Delhi View Details..</h1>
-                </div>
-               </button>
-            </div>
-            <button className='bg-[#5271ff] h-auto w-auto rounded-xl text-white font-bold p-2 hover:bg-blue-800'>Show Transit Details</button>
-        </div>
+        
        </div>
        
         {/* <LongCardImage field1={'Name'} value1={"Lekhram"}/> */}
-    </div>
+        <div className='flex flex-col space-y-4 h-full flex-wrap mx-4'>
+            <div className='bg-[#00bf63] w-auto h-auto p-2 rounded-xl font-bold justify-center flex items-centre'>
+                <h1 className='font-bold justify-items-center text-white items-centre flex text-lg'>In Transit</h1>
+            </div>
+            <div className='w-auto rounded-xl font-bold flex flex-col space-y-2 justify-center items-center' >
+               <button className='w-full'>
+               <LongCardTransit field1={'Driver Name'} value1={'Ramesh'} field2={'Vehicle'} value2={'CG 04 ZD 1643'} field3={'Item'} value3={'Rice'} field4={'Route'} value4={'Raipur-Delhi'} field5={'Invoice No'} value5={12} field6={'Legal Status'} value6={'Insured'}/>
+               </button>
+               
+            </div>
+            <button className='bg-[#5271ff] h-auto w-[30%] self-center rounded-xl text-white font-bold p-2 hover:bg-blue-800 mb-4'>Show Transit Details</button>
+        </div>
+ </div>
   ) 
 }
 
